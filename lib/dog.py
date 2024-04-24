0521db14a6bf49480bf536ad34fbca052772339d
reaganmunene <reagannmunene@gmail.com>
2024-04-24 09:20:21 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -15,25 +15,52 @@ class Dog:
         self.name = name
         self.breed = breed
 
-    def get_name(self):
+    # def get_name(self):
+    #     return self._name
+
+    @property
+    def name (self):
+        """the name property"""
         return self._name
 
-    def set_name(self, name):
+    # def set_name(self, name):
+    #     if isinstance(name, str) and 1 <= len(name) <= 25:
+    #         self._name = name.title()
+    #     else:
+    #         raise ValueError(
+    #             "Name must be string between 1 and 25 characters.")
+    @name.setter
+    def name(self,name):
+        """Name must be a string between 1 and 25 charachters in length"""
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 charachters.")
 
-    name = property(get_name, set_name)
 
-    def get_breed(self):
-        return self._breed
+    # name = property(get_name, set_name)
 
-    def set_breed(self, breed):
+    # def get_breed(self):
+    #     return self._breed
+
+    # def set_breed(self, breed):
+    #     if breed in APPROVED_BREEDS:
+    #         self._breed = breed
+    #     else:
+    #         raise ValueError("Breed must be in list of approved breeds.")
+
+    # breed = property(get_breed, set_breed)
+
+    @property
+    def breed(self, name):
+        return self._name
+
+    @breed.setter
+    def breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
-            raise ValueError("Breed must be in list of approved breeds.")
+            raise ValueError("Breed must be in the list of approved breeds.")
 
-    breed = property(get_breed, set_breed)
+
+# ipdb> Dog("Fido", "German shepherd")
